c2331afb11b79899b00b3c2adc9ccc01559121fa
Massil <achab@users.noreply.github.com>
2019-09-09 13:13:33 +0200

    compilation works

--- booking-contract/contracts/TimeTable.sol
+++ booking-contract/contracts/TimeTable.sol
@@ -1,62 +1,61 @@
 pragma solidity ^0.5.0;
 
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
-contract TimeTable {
+
+contract TimeTable is ERC721 {
 
     uint public n_timeslots;
     uint public n_rooms;
-    bytes32[] public owners;
-    bytes32[] public passwords;
     bool[] public availability;
 
-    event ReservationHasChanged(uint timeslot, uint room, bytes32 owner);
+    event ReservationHasChanged(uint timeslot, uint room);
 
-    constructor(uint n_timeslots_, uint n_rooms_) public {
+    constructor(uint n_timeslots_, uint n_rooms_) public ERC721() {
         n_timeslots = n_timeslots_;
         n_rooms = n_rooms_;
-        initTable(n_timeslots, n_rooms);
+        initTableTokens(n_timeslots, n_rooms);
     }
 
-    function initTable(uint r_rows, uint n_cols) internal {
+    function getTokenId(uint timeslot, uint room) public view returns (uint) {
+      // timeslot and room start at 0
+      uint id = room + timeslot * n_rooms;
+      return id;
+    }
+
+    function castBytes32Toaddres(bytes32 owner) internal pure returns (address) {
+      return address(uint160(bytes20(owner)));
+    }
+
+    function initTableTokens(uint r_rows, uint n_cols) internal {
       for (uint i = 0; i < r_rows; i++) {
         for (uint j = 0; j < n_cols; j++) {
-          owners.push("");
-          passwords.push("");
+          uint tokenId = getTokenId(i, j);
+          _mint(msg.sender, tokenId);
           availability.push(true);
         }
       }
     }
 
-    function getId(uint timeslot, uint room) public view returns (uint) {
-      // timeslot and room start at 0
-      uint id = timeslot * n_rooms + room;
-      return id;
+    function bookRoom(uint timeslot, uint room, bytes32 newOwner) public {
+      uint tokenId = getTokenId(timeslot, room);
+      address currentOwner = ownerOf(tokenId);
+      address newOwner_ = castBytes32Toaddres(newOwner);
+      approve(newOwner_, tokenId);
+      safeTransferFrom(currentOwner, newOwner_, tokenId);
+      availability[tokenId] = false;
+      emit ReservationHasChanged(timeslot, room);
     }
 
-    function bookRoom(uint timeslot, uint room, bytes32 owner, bytes32 password) public {
-      uint id = getId(timeslot, room);
-      require(availability[id], "The room is not available !");
-      availability[id] = false;
-      owners[id] = owner;
-      passwords[id] = password;
-      emit ReservationHasChanged(timeslot, room, owner);
-    }
-
-    function cancelReservation(uint timeslot, uint room, bytes32 owner, bytes32 password) public {
-      uint id = getId(timeslot, room);
-      require(owners[id] == owner, "You can not cancel a reservation you have not made !");
-      require(!availability[id], "The room has not been booked yet !");
-      require(passwords[id] == password, "The password is not correct !");
-      availability[id] = true;
-      emit ReservationHasChanged(timeslot, room, owner);
+    function cancelReservation(uint timeslot, uint room) public {
+      uint tokenId = getTokenId(timeslot, room);
+      require(!availability[tokenId], "The room has not been booked yet !");
+      availability[tokenId] = true;
+      emit ReservationHasChanged(timeslot, room);
     }
 
     function getAvailability() public view returns (bool[] memory) {
       return availability;
-    }
-
-    function getOwners() public view returns (bytes32[] memory) {
-      return owners;
     }
 
 }
